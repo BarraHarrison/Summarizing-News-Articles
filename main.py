30501fc51da146b9abc65f7f3b829d60f609074d
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-18 17:58:17 +0900

    adding button

--- main.py
+++ main.py
@@ -51,4 +51,24 @@ publication = tk.Text(root, height=1, width=140)
 publication.config(state='disabled', bg='#dddddd')
 publication.pack()
 
+slabel = tk.Label(root, text='Summary')
+slabel.pack()
+summary = tk.Text(root, height=20, width=140)
+summary.config(state='disabled', bg='#dddddd')
+summary.pack()
+
+selabel = tk.Label(root, text='Sentiment Analysis')
+selabel.pack()
+sentiment = tk.Text(root, height=1, width=140)
+sentiment.config(state='disabled', bg='#dddddd')
+sentiment.pack()
+
+ulabel = tk.Label(root, text='URL')
+ulabel.pack()
+utext = tk.Text(root, height=1, width=140)
+utext.pack()
+
+btn = tk.Button(root, text="Summarize")
+btn.pack()
+
 root.mainloop()
