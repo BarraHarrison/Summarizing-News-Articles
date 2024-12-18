2c96e7ec47a7f728d2b4c6096cb144d72c6ea423
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-18 17:14:30 +0900

    NLP work is done by the libraries

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import tkinter as tk
 import nltk 
 from textblob import TextBlob
-from newspaper import article
+from newspaper import Article
 
-# pip install nltk
-# pip install textblob
-# pip install newspaper3k
+nltk.download('punkt')
+
+url = 'https://edition.cnn.com/2023/04/02/us/tiktok-american-culture-effects-cec/index.html'
+
+# The NLP work is done by the libraries
+article = Article(url)
+article.download()
+article.parse()
+
+article.nlp()
+
+print(f'Title: {article.title}')
+print(f'Authors: {article.authors}')
+print(f'Publication Date: {article.publish_date}')
+print(f'Summary: {article.summary}')
