0e0598bd7bdc0c47d44900fa54dbfc79a5689bcd
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-18 18:24:45 +0900

    changing the GUI

--- main.py
+++ main.py
@@ -61,34 +61,35 @@ root.title("Python News Article Summarizer")
 root.geometry('1200x600')
 
 # Layout
+# Layout
 tlabel = tk.Label(root, text='Title')
 tlabel.pack()
-title = tk.Text(root, height=1, width=140, bg='#dddddd', state='disabled')
+title = tk.Text(root, height=1, width=140, bg='lightblue', state='disabled')
 title.pack()
 
 alabel = tk.Label(root, text='Author')
 alabel.pack()
-author = tk.Text(root, height=1, width=140, bg='#dddddd', state='disabled')
+author = tk.Text(root, height=1, width=140, bg='lightblue', state='disabled')
 author.pack()
 
 plabel = tk.Label(root, text='Publication Date')
 plabel.pack()
-publication = tk.Text(root, height=1, width=140, bg='#dddddd', state='disabled')
+publication = tk.Text(root, height=1, width=140, bg='lightblue', state='disabled')
 publication.pack()
 
 slabel = tk.Label(root, text='Summary')
 slabel.pack()
-summary = tk.Text(root, height=20, width=140, bg='#dddddd', state='disabled')
+summary = tk.Text(root, height=20, width=140, bg='lightblue', state='disabled')
 summary.pack()
 
 selabel = tk.Label(root, text='Sentiment Analysis')
 selabel.pack()
-sentiment = tk.Text(root, height=1, width=140, bg='#dddddd', state='disabled')
+sentiment = tk.Text(root, height=1, width=140, bg='lightblue', state='disabled')
 sentiment.pack()
 
 ulabel = tk.Label(root, text='URL')
 ulabel.pack()
-utext = tk.Text(root, height=1, width=140)
+utext = tk.Text(root, height=1, width=140, bg='lightblue')
 utext.pack()
 
 frame = tk.Frame(root)
